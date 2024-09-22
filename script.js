b88327384cd51c74788dceb633daedf8252c8bbf
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-09-22 17:30:58 -0500

    Consolidated all used images into the file rather than imgur albums. Still trying to find out how to enable that slider thing that worked on General Assembly but not on modern stuff

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+divElement = document.querySelectorAll('div')
+
 $('div').on('click', function() {
     $(this).toggleClass('show-description');
     // Perhaps .toggle('show-description') will do the trick. Check on my free time...
 });
-    
