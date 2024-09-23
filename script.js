082628e96b01e5afcfb6433ed6c1cfa4cc30dbb4
ShirtHanger <52720578+ShirtHanger@users.noreply.github.com>
2024-09-23 12:22:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ $('div').on('click', function() {
     // Perhaps .toggle('show-description') will do the trick. Check on my free time...
     // Nope. Try something like document.body.classList.toggle('green')
 });
+
+// Alteratively, try:
+
+// const setColor = (colorClass) => () => { // Ashley said this would fix it, no clue how...
+//     document.body.classList.toggle(colorClass)
+//     // This makes it an anonymous function, meaning it must be specifically
+//     // Called to execute, not execute immedietly
+//     // Think of it as a container, will not execute
+        // Unless you call it
+// }
