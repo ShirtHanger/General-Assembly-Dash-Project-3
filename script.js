4c6894503ea33ac0e0e5c8162a4b18033e6fb862
ShirtHanger <52720578+ShirtHanger@users.noreply.github.com>
2024-09-23 09:24:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ divElement = document.querySelectorAll('div')
 $('div').on('click', function() {
     $(this).toggleClass('show-description');
     // Perhaps .toggle('show-description') will do the trick. Check on my free time...
+    // Nope. Try something like document.body.classList.toggle('green')
 });
