5798c3d0fdc085d843bbfb22541141884857a948
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-09-30 19:20:40 -0500

    Still trying to figure out why this wont work

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-divElement = document.querySelectorAll('div')
-
-$('div').on('click', function() {
-    $(this).toggleClass('show-description');
-    // Perhaps .toggle('show-description') will do the trick. Check on my free time...
-    // Nope. Try something like document.body.classList.toggle('green')
-});
-
 // Alteratively, try:
 
 // const setColor = (colorClass) => () => { // Ashley said this would fix it, no clue how...
@@ -15,3 +7,24 @@ $('div').on('click', function() {
 //     // Think of it as a container, will not execute
         // Unless you call it
 // }
+
+
+divElements = document.querySelectorAll('div')
+
+for (divElement of divElements) {
+    divElement.addEventListener('click', () => {
+        alert('Click!')
+        divElement.classList.toggle('show-description')
+    })
+}
+
+/* STILL WONT WORK! */
+
+/* Here's the original code that General Assembly Dash told me to use for this
+
+ $('div').on('click', function() {
+     $(this).toggleClass('show-description');
+ });
+
+Outdated  af apparently. Couldn't figure out why it worked on Dash but not my computer until I learned about DOM manipulation
+in the actual General Assembly Bootcamp */
